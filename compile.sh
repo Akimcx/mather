ca2547150321d69740a974b9eb0e7f98e7b86802
Akim <silverdleo@protonmail.com>
2024-03-19 12:13:24 -0400

    chore: Add picocli to the project in order to have less moving part

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #/usr/bin/env bash
 
 rm -rf bin
-javac -d bin/main $(find src/main -name "*.java")
+javac -d bin/main -cp ./lib/picocli-4.7.5.jar $(find src/main -name "*.java")
 # javac -d bin/test -cp bin/main/cx.ksim.mather.cli/ src/test/cx.ksim.mather.cli/cx/ksim/mather/test/LexerTest.java src/test/cx.ksim.mather.cli/cx/ksim/mather/test/ParserTest.java
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env bash
 
-java -cp bin/main cx.ksim.mather.cli.Parser $@
+java -cp bin/main:./lib/picocli-4.7.5.jar cx.ksim.mather.cli.Parser $@
