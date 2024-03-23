f4f17be7ae0573b70c9f5274c8352f17cd73ceb9
Akim <silverdleo@protonmail.com>
2024-03-23 00:36:24 -0400

    chore(test): Running test from cli seems to work

--- compile.sh
+++ compile.sh
@@ -1,5 +1,11 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
+
+set -x
 
 rm -rf bin
 javac -d bin/main -cp ./lib/picocli-4.7.5.jar $(find src/main -name "*.java")
+
+if [ "$1" = "-t" ]; then
+    javac -d bin/main -cp bin/main:./lib/junit-platform-console-standalone-1.10.0.jar $(find src/test -name "*.java")
+fi
 # javac -d bin/test -cp bin/main/cx.ksim.mather.cli/ src/test/cx.ksim.mather.cli/cx/ksim/mather/test/LexerTest.java src/test/cx.ksim.mather.cli/cx/ksim/mather/test/ParserTest.java
--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
-java -cp bin/main:./lib/picocli-4.7.5.jar cx.ksim.mather.cli.Parser $@
+set -x
+
+if [ "$1" = "-t" ]; then
+    java -jar ./lib/junit-platform-console-standalone-1.10.0.jar -cp bin/main --select-class "cx.ksim.mather.cli.${2}"
+else
+    java -cp bin/main:./lib/picocli-4.7.5.jar cx.ksim.mather.cli.Parser "$@"
+fi
